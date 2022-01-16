d29da244723c3212b4d2bcfaf68524a0f9425f6b
vijayrjdev@gmail.com <vijayrajck@outlook.com>
2022-01-16 13:21:22 +0530

    Login with wallet

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,36 @@
 import Image from "next/image";
+import { useMoralis } from "react-moralis";
 
 const Login = () => {
+  const { authenticate } = useMoralis();
+
   return (
-    <div>
-      <div></div>
-      <h1>I'm login</h1>
+    <div className="bg-black relative h-screen">
+      <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+        <Image
+          src="https://images.unsplash.com/photo-1642104704074-907c0698cbd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
+          alt=""
+          height={200}
+          width={200}
+          objectFit="cover"
+          className="rounded-full"
+        />
+        <button
+          className="bg-yellow-500 rounded-lg p-4 font-bold animate-pulse"
+          onClick={authenticate}
+        >
+          Login to METAVERSE
+        </button>
+      </div>
+
+      <div className="-z-50">
+        <Image
+          src="https://links.papareact.com/55n"
+          alt=""
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
     </div>
   );
 };
